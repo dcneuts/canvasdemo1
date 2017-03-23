0ea5de6b1a371cb4858bc49c9f658c8f5fc8a87f
dcneuts <dcneuts@gmail.com>
2017-03-23 00:40:21 -0700

    Initial commit for Treehouse exercise

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts cause changes appropriately
+
+//When you click control list the items
+  //Deselect sibling elements
+  //Select clicked elements
+
+//When add color button is pressed
+  //
