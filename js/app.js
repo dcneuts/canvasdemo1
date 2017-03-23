e60115ae03eace78130a7fd880dcb11b34c3e217
dcneuts <dcneuts@gmail.com>
2017-03-23 02:14:05 -0700

    Attempting to correct an issue with formatting that is causing problems with Chrome and Opera to properly display color changes.

--- js/app.js
+++ js/app.js
@@ -3,35 +3,35 @@
 var color = $(".selected").css("background-color");
 
 //When you click control list the items
-$(".controls li").click(function() {
-  //Deselect sibling elements
-$(this).siblings().removeClass("selected");
-  //Select clicked elements
-$(this).addClass("selected");
-  //Cache the current color
-  color=$(this).css("background-color");
+$(".controls li").click(function () {
+	//Deselect sibling elements
+	$(this).siblings().removeClass("selected");
+	//Select clicked elements
+	$(this).addClass("selected");
+	//Cache the current color
+	color = $(this).css("background-color");
 });
 
 //When new color button is pressed
-$("#revealColorSelect").click(function(){
-    //Show color Select or hide color select
-    $("#colorSelect").toggle();
+$("#revealColorSelect").click(function () {
+	//Show color Select or hide color select
+	$("#colorSelect").toggle();
 });
 
-function changeColor(){
-    //When the color is changed update the BG color span
-    var r = $("#red").val();
-    var g = $("#green").val();
-    var b = $("#blue").val();
-    $("newColor").css("background-color", "rgb(" + r + "," + g +"," + b +")");
+function changeColor() {
+	//When the color is changed update the BG color span
+	var r = $("#red").val();
+	var g = $("#green").val();
+	var b = $("#blue").val();
+	$("newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
 //Slider changes
 $("input[type=range]").on("input", changeColor);
 
 //When add color is pressed
-  //Append color to the control ul
-  //Select the new color
+//Append color to the control ul
+//Select the new color
 
 //On mouse events on canvas
-  //Draw some lines using JS
+//Draw some lines using JS
